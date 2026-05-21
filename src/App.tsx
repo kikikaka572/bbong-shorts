1dc44e0a7f135a6581aaa85851ec61985ce29c40
unknown <G00210@greencar.net>
2026-05-21 16:01:54 +0900

    fix: BrowserRouter basename 추가 (GitHub Pages 경로 수정)

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Index from "./pages/Index";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/bbong-shorts/">
       <Routes>
         <Route path="/" element={<Index />} />
       </Routes>
